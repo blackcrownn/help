844cff159d374189fceb7261c92f7abd092f0c73
Ceose <orhuncagdas200@gmail.com>
2024-07-18 16:06:13 +0300

    --

--- help/src/main/java/help/help/module/Kullanicilar.java
+++ help/src/main/java/help/help/module/Kullanicilar.java
@@ -1,4 +1,39 @@
 package help.help.module;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Kullanicilar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column()
+    private String kullaniciAdi;
+
+    @Column()
+    private String kullaniciSifre;
+
+    @Column
+    private String email;
+
+    public Kullanicilar(int id, String kullaniciAdi, String kullaniciSifre, String email) {
+        this.id = id;
+        this.kullaniciAdi = kullaniciAdi;
+        this.kullaniciSifre = kullaniciSifre;
+        this.email = email;
+    }
+
+    public Kullanicilar() {
+    }
+
+    @Override
+    public String toString() {
+        return "Kullanicilar{" +
+                "id=" + id +
+                ", kullaniciAdi='" + kullaniciAdi + '\'' +
+                ", kullaniciSifre='" + kullaniciSifre + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
---
+++ help/src/main/java/help/help/repository/KullanicilarRepository.java
@@ -0,0 +1,8 @@
+package help.help.repository;
+
+import help.help.module.Kullanicilar;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KullanicilarRepository extends JpaRepository<Kullanicilar, Integer> {
+
+}
