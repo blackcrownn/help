23035b5341e41974930af52463b5fa804afc83bf
Berke Erkan Doğan <berkan.dogan@gazi.edu.tr>
2024-07-22 14:07:16 +0300

    html code transform
    Text to html

--- help.client/src/components/TextEditor.tsx
+++ help.client/src/components/TextEditor.tsx
@@ -1,33 +1,41 @@
-import React, { useCallback, useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
+import ReactQuill from 'react-quill';
 
 
 
 //Rich Text Editor 
 export default function TextEditor() {
     
-
-    
-
-    const wrapperRef = useCallback(wrapper => { 
+    /*const wrapperRef = useCallback(wrapper => { 
     if (wrapper == null) return
 
         wrapper.innerHTML = ""
         const editor = document.createElement('div');
         wrapper.append(editor);
         new Quill(editor, {theme:'snow'});
-    }, []);
+    }, []); */
 
 
-    return <div id='container' ref={wrapperRef}>
-            <p>{}</p>
+    //const quill = new Quill('#editor');
 
-    </div> 
-       
+    //const html = quill.root.innerHTML;
+
+    const [editorHtml, setEditorHtml] = useState('');
+
+    const handleChange = (content, delta, source, editor) => {
+        setEditorHtml(editor.getHTML()); // HTML formatında içeriği günceller
+    };
          
-       
+    return (
+        <div id="editor">
+            <ReactQuill theme="snow" onChange={handleChange} />
+            <div>{`HTML code is ${editorHtml}`}</div>
+        </div>
+    );
+};
          
 
-}
+
   
